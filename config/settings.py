016b1d73eda0e3e677e3056fdeb2a66de9e6ccd4
sayfullayev0204 <sayfullayevgayrat7@gmail.com>
2024-11-20 02:18:51 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://teachapp-production.up.railway.app']
 
 
 # Application definition
