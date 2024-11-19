1d8e5c99cb8f2e21d250541c3cdaefdf58ebe83b
sayfullayev0204 <sayfullayevgayrat7@gmail.com>
2024-11-20 02:38:58 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -52,15 +52,7 @@ INSTALLED_APPS = [
     'drf_yasg'
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
+
 
 
 MIDDLEWARE = [
